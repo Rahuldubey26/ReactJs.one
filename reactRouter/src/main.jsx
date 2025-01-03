28096f4994f29c62d0d9e9952571909b7237fb51
Rahul Dubey <111676105+Rahuldubey26@users.noreply.github.com>
2025-01-03 03:43:49 +0000

    Worked on ReactRouter and build a small website for pratice

---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,55 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import AboutUs from './components/AboutUs/AboutUs.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path: "about",
+//         element: <AboutUs/>
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home/>} />
+      <Route path='about' element={<AboutUs/>} />
+      <Route path='contact' element={<Contact/>} />
+      <Route path='user/:userid' element={<User/>} />
+      <Route 
+      path='github'
+       element={<Github/>}
+       loader={()=>{
+        githubInfoLoader
+       }}
+       />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
