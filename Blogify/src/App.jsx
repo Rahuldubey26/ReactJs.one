6ff12e9d27d9ca72096f158f1cd4ebf7830a4e4d
Rahul Dubey <111676105+Rahuldubey26@users.noreply.github.com>
2025-01-20 10:57:32 +0000

    worked on store and authSlice:

--- Blogify/src/App.jsx
+++ Blogify/src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL)
-  return (
-    <>
-      <h1>APP with APPWrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const displatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          displatch(login({
+            userData
+          }))
+        }
+        else {
+          displatch(logout());
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet/>
+        </main>
+        <Footer />
+      </div>
+    </div>
+
+  ) : (null)
 }
 
 export default App
--- Blogify/src/appwrite/auth.js
+++ Blogify/src/appwrite/auth.js
@@ -1,7 +1,7 @@
 
 import conf from '../conf/conf'
 import { Client, Account, ID } from "appwrite"
-import { AuthService } from './auth';
+
 
 export class AuthService {
     client = new Client();
